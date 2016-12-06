4639f9a945c8aa054bd81904dfc0bceb4aeabaac
Hassan Mughal <hassan.mughal@gmail.com>
2016-12-06 17:01:26 +0300

    Made req, res param names uniform.

--- app.js
+++ app.js
@@ -85,9 +85,9 @@ app.get('/auth/sso/callback', function(req, res, next) {
     })(req,res,next);
 });
 
-app.get('/hello', ensureAuthenticated, function(request, response) {
+app.get('/hello', ensureAuthenticated, function(req, res) {
     res.send('Hello, '+ request.user['id'] + '!\n' + '<a href="/logout">Log Out</a>');
-});
+});	
 
 app.get('/logout', function(req, res){
     req.logout();
