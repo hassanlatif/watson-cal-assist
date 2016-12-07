3e5f381201ba97fe63058a5369db27362fa6e454
Hassan Mughal <hassan.mughal@gmail.com>
2016-12-07 10:56:41 +0300

    revise Strategy

--- app.js
+++ app.js
@@ -1,95 +1,114 @@
-// Licensed under the Apache License. See footer for details.
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
 var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+//other dependencies to use SSO
 var passport = require('passport');
-var app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser());
-
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
+
+// create a new express server
+var app = express();
 app.use(cookieParser());
-
-var expressSession = require('express-session');
-var sessionStore  = new expressSession.MemoryStore;
-app.use(expressSession({ secret: 'somesecretmagicword', store: sessionStore}))
-
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
+   done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+   done(null, obj);
 });
 
-var port = (process.env.VCAP_APP_PORT || 8192);
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-var url = JSON.parse(process.env.VCAP_APPLICATION || '{"uris":["' + 'https://' + host + ':' + port + '"]}').uris[0] 
 
-var SSO_CLIENT_ID = (process.env.SSO_CLIENT_ID || ' ');
-var SSO_CLIENT_SECRET = (process.env.SSO_CLIENT_SECRET || ' ');
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+var ssoConfig = services.SingleSignOn[0]; 
+var client_id = ssoConfig.credentials.clientId;
+var client_secret = ssoConfig.credentials.secret;
+var authorization_url = ssoConfig.credentials.authorizationEndpointUrl;
+var token_url = ssoConfig.credentials.tokenEndpointUrl;
+var issuer_id = ssoConfig.credentials.issuerIdentifier;
+var callback_url = 'https://watson-cal-assist.mybluemix.net/auth/sso/callback';
+ 
+var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
+var Strategy = new OpenIDConnectStrategy({
+                authorizationURL : authorization_url,
+                tokenURL : token_url,
+                clientID : client_id,
+                scope : 'email',
+                response_type : 'code',
+                clientSecret : client_secret,
+                callbackURL : callback_url,
+                skipUserProfile : true,
+                issuer : issuer_id},
+      function(iss, sub, profile, accessToken, refreshToken, params, done) {
+        process.nextTick(function() {
+            profile.accessToken = accessToken;
+            profile.refreshToken = refreshToken;
+            done(null, profile);
+        })
+      }
+})
+ 
+passport.use(Strategy); 
+app.get('/login', passport.authenticate('openidconnect', {})); 
+ 
 
-var IbmIdStrategy = require('passport-ibmid-oauth2').Strategy;
-passport.use('ibmid', new IbmIdStrategy({
-    clientID: SSO_CLIENT_ID,
-    clientSecret: SSO_CLIENT_SECRET,
-    callbackURL: 'https://' + url + '/auth/ibmid/callback',
-    passReqToCallback: true    
-  }, function(req, accessToken, refreshToken, profile, done) {
-    req.session.ibmid = {};
-    req.session.ibmid.profile = profile;
-    return done(null, profile);
-  }
-));
-
-app.get('/auth/ibmid', passport.authenticate('ibmid', { scope: ['profile'] }), function(req, res) {
-});
-
-app.get('/auth/ibmid/callback', passport.authenticate('ibmid', { failureRedirect: '/error', scope: ['profile'] }), function(req, res) {
-  res.redirect('/private')
-});        
-
-function authenticate() {
-  return function(req, res, next) {
-    if (!req.isAuthenticated() || req.session.ibmid == undefined)
-      res.redirect('/auth/ibmid');
-    else
-      next();
+function ensureAuthenticated(req, res, next) {
+  if(!req.isAuthenticated()) {
+              req.session.originalUrl = req.originalUrl;
+    res.redirect('/login');
+  } else {
+    return next();
   }
 }
 
-app.get('/', function(req, res) {
-  res.send('Hello World! <a href="/private">Login</a>\n');
+app.get('/auth/sso/callback',function(req,res,next) {
+    var redirect_url = req.session.originalUrl;
+        passport.authenticate('openidconnect', {
+                successRedirect: '/hello',
+                failureRedirect: '/failure',
+        })(req,res,next);
+    });
+ 
+
+
+app.get('/hello', ensureAuthenticated, function(req, res) {
+	console.log(req.user);
+    res.send('Hello, '+ req.session.user['id'] + '!\n' + '<a href="/logout">Log Out</a>');
 });
 
-app.get('/private', authenticate(), function(req, res) {
-  var profile = req.session.ibmid.profile;
-  res.send('Hello ' + profile.firstName + ' ' + profile.lastName + '! <a href="/logout">Logout</a>\n');
+app.get('/logout', function(req, res){
+    req.logout();
+    //res.redirect('/');
+     res.redirect('https://' + issuer_id +
+                  '/idaas/mtfim/sps/idaas/logout');
 });
 
-app.get('/error', function(req, res) {
-  res.send('Failed to authenticate\n');
+app.get('/failure', function(req, res) { 
+             res.send('login failed'); });
+
+ 
+app.get('/', function (req, res) {
+    res.send('<h1>Bluemix Service: Single Sign On</h1>' + '<p>Sign In with a Social Identity Source (SIS): Cloud directory, Facebook, Google+ or LinkedIn.</p>' + '<a href="/auth/sso/callback">Sign In with a SIS</a>');
 });
 
-app.get('/logout', function(req, res) {
-  passport._strategy('ibmid').logout(req, res, 'https://' + url + '/');
-});
+var appEnv = cfenv.getAppEnv();
 
-app.listen(port, host);
-//-------------------------------------------------------------------------------
-// Copyright IBM Corp. 2014
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//-------------------------------------------------------------------------------
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
